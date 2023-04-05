168101a4162b6fe2d67faa3d4f33467c7d5b8e5c
Paloma Baker <paloma.d.baker@gmail.com>
2023-04-05 15:09:44 -0700

    added the Helmet middleware to the server.js file

--- server-start/server.js
+++ server-start/server.js
@@ -1,15 +1,22 @@
 const express = require('express');
+const cookieParser = require('cookie-parser');
 const routes = require('./routes');
 const sequelize = require('./config/connection');
+const helmet = require('helmet');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+//helmet middleware to set HTTP headers for improved security 
+app.use(helmet());
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 
-app.use(routes);
+//placing the helmet middleware before the routes to make sure it is executed for all requests
+app.use('/', routes);
 
 
 sequelize.sync({ force: false }).then(() => {
