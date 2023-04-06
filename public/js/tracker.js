9b4fdf690e5f61e0246dba00822890dd7658e6e8
Ronasian <119774818+Ronasian@users.noreply.github.com>
2023-04-05 18:50:24 -0700

    set up html, css, and js for tracker page: take user inputs, converts units to be passed to API, response from API is an object with caloric intakes for weight gain/loss, info is rendered under results on tracker page

--- public/js/tracker.js
+++ public/js/tracker.js
@@ -3,6 +3,8 @@ const feet = document.getElementById('feet');
 const inches = document.getElementById('inches');
 const weight = document.getElementById('weight');
 const calculateBtn = document.getElementById('calculate');
+const calList = document.getElementById('calories');
+const calDiv = document.getElementById('results');
 
 function convertHeight(feet, inches) {
 	let height = (feet * 12) + inches;
@@ -29,6 +31,26 @@ calculateBtn.addEventListener('click', (e) => {
 	    .then(response => response.json())
 	    .then(response => {
 			console.log(response);
+
+			let maintainEl = document.createElement('li');
+			let gainOneEl = document.createElement('li');
+			let gainTwoEl = document.createElement('li');
+			let loseOneEl = document.createElement('li');
+			let loseTwoEl = document.createElement('li');
+
+			maintainEl.textContent = `You need a daily intake of ${response.calories} calories to maintain your current weight each week`;
+			gainOneEl.textContent = `You need a daily intake of ${response.gain_500g_per_week} calories to gain about 1 pound per week`;
+			gainTwoEl.textContent = `You need a daily intake of ${response.gain_1kg_per_week} calories to gain about 2 pounds per week`;
+			loseOneEl.textContent = `You need a daily intake of ${response.lose_500g_per_week} calories to lose around 1 pound per week`;
+			loseTwoEl.textContent = `You need a daily intake of ${response.lose_1kg_per_week} calories to lose around 2 pounds per week`;
+			
+			calList.appendChild(maintainEl);
+			calList.appendChild(gainOneEl);
+			calList.appendChild(gainTwoEl);
+			calList.appendChild(loseOneEl);
+			calList.appendChild(loseTwoEl);
+
+			calDiv.classList.remove('hidden');
 		})
 	    .catch(err => console.error(err));
-})
+});
