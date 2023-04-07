cf02035bf02c3d631b58b1115eff28f9be47abed
Ronasian <119774818+Ronasian@users.noreply.github.com>
2023-04-06 17:56:33 -0700

    finished tracker page

--- public/js/tracker.js
+++ public/js/tracker.js
@@ -5,6 +5,11 @@ const weight = document.getElementById('weight');
 const calculateBtn = document.getElementById('calculate');
 const calList = document.getElementById('calories');
 const calDiv = document.getElementById('results');
+const maintainEl = document.getElementById('maintain');
+const gainOneEl = document.getElementById('gainOne');
+const gainTwoEl = document.getElementById('gainTwo');
+const loseOneEl = document.getElementById('loseOne');
+const loseTwoEl = document.getElementById('loseTwo');
 
 function convertHeight(feet, inches) {
 	let height = (feet * 12) + inches;
@@ -32,23 +37,11 @@ calculateBtn.addEventListener('click', (e) => {
 	    .then(response => {
 			console.log(response);
 
-			let maintainEl = document.createElement('li');
-			let gainOneEl = document.createElement('li');
-			let gainTwoEl = document.createElement('li');
-			let loseOneEl = document.createElement('li');
-			let loseTwoEl = document.createElement('li');
-
 			maintainEl.textContent = `You need a daily intake of ${response.calories} calories to maintain your current weight each week`;
 			gainOneEl.textContent = `You need a daily intake of ${response.gain_500g_per_week} calories to gain about 1 pound per week`;
 			gainTwoEl.textContent = `You need a daily intake of ${response.gain_1kg_per_week} calories to gain about 2 pounds per week`;
 			loseOneEl.textContent = `You need a daily intake of ${response.lose_500g_per_week} calories to lose around 1 pound per week`;
 			loseTwoEl.textContent = `You need a daily intake of ${response.lose_1kg_per_week} calories to lose around 2 pounds per week`;
-			
-			calList.appendChild(maintainEl);
-			calList.appendChild(gainOneEl);
-			calList.appendChild(gainTwoEl);
-			calList.appendChild(loseOneEl);
-			calList.appendChild(loseTwoEl);
 
 			calDiv.classList.remove('hidden');
 		})
